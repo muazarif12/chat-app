4a039b23b45fad7d196e4549a1655b8b94dfd3ad
muazarif12 <maaz.arif12@gmail.com>
2025-08-13 16:19:32 +0500

    Update profile api and check authenticated api implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,8 @@
 import express from "express"
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { checkAuth, signup, login, logout } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+
+
 const router = express.Router();
 
 router.post("/signup",signup);
@@ -10,6 +12,9 @@ router.post("/login",login);
 router.get("/logout",logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth)
+
 
 
 export default router;

d9b04cc910a51ef0268fb09f484870434d6b90fd
muazarif12 <maaz.arif12@gmail.com>
2025-08-13 16:08:58 +0500

    Protected Route middleware implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
 
 const app = express();
@@ -8,7 +9,8 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup",signup);
@@ -7,6 +8,8 @@ router.post("/signup",signup);
 router.post("/login",login);
 
 router.get("/logout",logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
 
 
 export default router;

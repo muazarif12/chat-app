78c27e99d9d06cd7ec1acd094bbe2f157ba681da
muazarif12 <maaz.arif12@gmail.com>
2025-08-14 23:59:04 +0500

    message model implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
@@ -11,7 +13,9 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port PORT:${PORT}`)

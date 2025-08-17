9fd27e71ef7431d43ab9e5f69f4fda4c37fdbf7e
muazarif12 <maaz.arif12@gmail.com>
2025-08-18 01:26:12 +0500

    debugging error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,6 @@ const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore()
   const {theme} = useThemeStore()
 
-  console.log({onlineUsers})
 
   useEffect(() => {
     checkAuth()

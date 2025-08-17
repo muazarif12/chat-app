c63503d9e31c5aca23a7fe38801411f7385dd9fb
muazarif12 <maaz.arif12@gmail.com>
2025-08-18 00:22:29 +0500

    formatMessageTime

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -64,7 +64,7 @@ const ChatContainer = () => {
               </div>
             </div>
             <div className="chat-header mb-1">
-              <time className="text-xs opacity-50 ml-1">{formatMessageTime(message.createdAt)}</time>
+              <time className="text-xs opacity-50 ml-1">{formatMessageTime(message.createdAt) }</time>
             </div>
             <div className="chat-bubble flex flex-col">
               {message.image && (
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,5 @@
-export function formatMessageTime(date: string | number | Date) {
+export function formatMessageTime(date?: Date ) {
+  if (!date) return "";
   return new Date(date).toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",

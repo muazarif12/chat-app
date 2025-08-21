504a2c253acb6c1ebbbfb9d09c499e17da8c6c03
muazarif12 <maaz.arif12@gmail.com>
2025-08-21 23:30:22 +0500

    chat header changes

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -1,10 +1,10 @@
 import { X } from "lucide-react";
-import { useAuthStore } from "../store/useAuthStore";
+// import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 
 const ChatHeader = () => {
   const { selectedUser, setSelectedUser } = useChatStore();
-  const { onlineUsers } = useAuthStore();
+  // const { onlineUsers } = useAuthStore();
   if (!selectedUser) {
     return null; // or return a loading state/empty header
   }
